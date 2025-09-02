de87287963534fa0ed83103597fe83a392145f9d
AmZu1212 <zuabiamir123@gmail.com>
2025-09-02 11:42:41 +0000

    fixed typo

--- Project Files/Benchmark 1 - JSON_Dumps/runB1.sh
+++ Project Files/Benchmark 1 - JSON_Dumps/runB1.sh
@@ -5,17 +5,17 @@ set -e  # Exit on error
 # Authored by Amir Zuabi & Nir Schif
 
 
-# make sure you update the directory which your flamegraph is installed at, and the directory where the pyperformance benchmarks are located. 
+# make sure you update the directory which your flamegraph is installed at, and the directory where the pyperformance benchmarks are located.
 # usually in: /usr/local/lib/python3.10/dist-packages/pyperformance/data-files/benchmarks/bm_json_dumps/
 # for python 3.X you'll need to change the number obviously.
 
 
-# this scirpt runs both versions of the benchmark, json std and json orjson. 
+# this script runs both versions of the benchmark, json std and json orjson.
 # makes the flamegraphs for you, and outputs the perfs in the perf outputs folder.
 
-# to see the stats in the console liek we showcased in out project document, we used this line. 
-# unfortunately you cannot create a Flamegraph and showcase these stats at the same time, 
-# so we left it out of the script. you can use this command and some editing of this script to make it appear, 
+# to see the stats in the console liek we showcased in out project document, we used this line.
+# unfortunately you cannot create a Flamegraph and showcase these stats at the same time,
+# so we left it out of the script. you can use this command and some editing of this script to make it appear,
 # or just do it manually with your prefered version (json/orjson) and see the stats :)
 
 
