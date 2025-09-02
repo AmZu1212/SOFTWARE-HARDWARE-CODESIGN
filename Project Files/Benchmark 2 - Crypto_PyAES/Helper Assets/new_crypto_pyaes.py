8cd9a2556e2e6b6cbd1c61633e5fc02cec3c3dd5
AmZu1212 <zuabiamir123@gmail.com>
2025-09-02 12:59:19 +0000

    added sandbox file for edits

---
+++ Project Files/Benchmark 2 - Crypto_PyAES/Helper Assets/new_crypto_pyaes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+
+# A version to make edits and performance optimizations.
+
+import pyperf
+
+import pyaes
+
+
+# 23,000 bytes in size
+CLEARTEXT = b"This is a test. What could possibly go wrong? " * 500
+
+# 128-bit key (16 bytes)
+KEY = b'\xa1\xf6%\x8c\x87}_\xcd\x89dHE8\xbf\xc9,'
+
+
+def bench_pyaes(loops):
+    range_it = range(loops)
+    t0 = pyperf.perf_counter()
+
+    # benchmark workload
+    for loops in range_it:
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        ciphertext = aes.encrypt(CLEARTEXT)
+
+        # need to reset IV for decryption
+        aes = pyaes.AESModeOfOperationCTR(KEY)
+        plaintext = aes.decrypt(ciphertext)
+
+        # explicitly destroy the pyaes object
+        aes = None
+
+    dt = pyperf.perf_counter() - t0
+    if plaintext != CLEARTEXT:
+        raise Exception("decrypt error!")
+
+    return dt
+
+
+# some pyperf boilerplate, ignore.
+if __name__ == "__main__":
+    runner = pyperf.Runner()
+    runner.metadata['description'] = ("Pure-Python Implementation "
+                                      "of the AES block-cipher")
+    runner.bench_time_func('crypto_pyaes', bench_pyaes)
